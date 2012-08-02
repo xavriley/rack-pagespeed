e53792e0a6572ac139c5446bfa949a6fb547856f
Xavier Riley <xavier@kyanmedia.com>
2012-08-02 22:48:05 +0100

    proof of concept

--- lib/rack/pagespeed/filters/seo_meta.rb
+++ lib/rack/pagespeed/filters/seo_meta.rb
@@ -1,12 +1,16 @@
 class Rack::PageSpeed::Filters::SeoMeta < Rack::PageSpeed::Filter
   requires_store
-  name    'seo_meta'
   priority 2
   
   def execute! document
     nodes = document.css('h1')
-    doc_title = document.css('title')
+    doc_title = document.css('title').first
     return false unless nodes.count > 0
-    doc_title.text = nodes.map{|h1| h1.text() }.join(' ')
+    new_title = doc_title.clone
+    new_title.content = nodes.map{|h1| h1.text() }.join(' ')
+    doc_title.before new_title
+    doc_title.remove
   end
 end
+
+
