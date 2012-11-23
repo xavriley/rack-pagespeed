b2e22c916abd4999e24af22e9171999ed1a8365b
Xavier Riley <xavier@kyanmedia.com>
2012-11-23 21:47:30 +0000

    Handle missing meta tags better

--- lib/rack/pagespeed/filters/seo_meta.rb
+++ lib/rack/pagespeed/filters/seo_meta.rb
@@ -6,33 +6,52 @@ class Rack::PageSpeed::Filters::SeoMeta < Rack::PageSpeed::Filter
   priority 2
   
   def execute! document
-    nodes = document.css('h1')
-    doc_title = document.css('title').first
-    return false unless nodes.count > 0
-    new_title = doc_title.clone
-    new_title.content = nodes.map{|h1| h1.text() }.join(' ')
-    doc_title.before new_title
-    doc_title.remove
+    @options[:title] ||= "%{h1}"
+    @options[:meta_desc] ||= "%{body}"
+    @options[:meta_keywords] ||= "%{body}"
 
-    body_text = document.css('#main').inner_text.gsub(/\s+/, ' ')
+    title_format = (@options[:meta_title] || "%{h1}")
+
+    title_format.gsub!(/%{([^\}]+)}/) do 
+      "#{document.css($1).first.try(:'text')}"
+    end
+
+    if 
+      doc_title = document.css('title').first 
+    else 
+      doc_title = Nokogiri::XML::Element.new('title', document)
+      document.at('head').children.first.before doc_title
+    end
+    doc_title.content = title_format
+
+    description_selector = @options[:meta_desc]
+    body_text = description_selector.gsub!(/%{([^\}]+)}/) do 
+      "#{document.css($1).first.try(:'inner_text')}".gsub(/\s+/, ' ')
+    end
 
     new_meta_desc_text = body_text.summarize(:ratio => 10)
-    meta_desc = document.css("meta[name='description']").first
-    new_meta_desc = meta_desc.clone
-    new_meta_desc['content'] = new_meta_desc_text
-    meta_desc.before new_meta_desc
-    meta_desc.remove
+    if 
+      meta_desc = document.css("meta[name='description']").first
+    else
+      meta_desc = Nokogiri::XML::Element.new('meta', document)
+      meta_desc['name'] = "description"
+      document.at('head').children.first.after meta_desc
+    end
+    meta_desc['content'] = new_meta_desc_text
 
-    new_meta_keywords_text = body_text.summarize(:topics => true)
-    meta_keywords = document.css("meta[name='keywords']").first
-    meta_keywords ||= Nokogiri::XML::Element.new('meta', document)
-    meta_keywords['name'] = "keywords"
+    keyword_selector = @options[:meta_keywords]
+    new_meta_keywords_text = keyword_selector.gsub!(/%{([^\}]+)}/) { 
+      "#{document.css($1).first.try(:'inner_text')}".gsub(/\s+/, ' ')
+    }.summarize(:topics => true)
+
+    if 
+      meta_keywords = document.css("meta[name='keywords']").first
+    else
+      meta_keywords = Nokogiri::XML::Element.new('meta', document)
+      meta_keywords['name'] = "keywords"
+      document.at('head').children[1].after meta_keywords
+    end
     meta_keywords['content'] = new_meta_keywords_text.last.to_s
-
-    new_meta_keywords = meta_keywords.clone
-    new_meta_keywords['content'] = new_meta_keywords_text.last.to_s
-    new_title.after new_meta_keywords
-    meta_keywords.remove
 
   end
 
